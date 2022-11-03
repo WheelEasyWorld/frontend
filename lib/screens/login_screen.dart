9789810ba6a251500551812c5b95b96a065c93c6
thing-zoo <cathy4025@gmail.com>
2022-11-03 17:22:52 +0100

    feat: add login screen

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:frontend/widgets/sns_button.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 35.w),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/images/logo.png'),
+            SNSButton(
+              onPressed: () {},
+              icon: 'kakao',
+              label: '카카오톡으로 계속하기',
+              btnColor: const Color(0xFFFBE300),
+            ),
+            SizedBox(height: 13.h),
+            SNSButton(
+              onPressed: () {},
+              icon: 'naver',
+              label: '네이버로 계속하기',
+              btnColor: const Color(0xFF03CF5D),
+              textColor: Colors.white,
+            ),
+            SizedBox(height: 13.h),
+            SNSButton(
+              onPressed: () {},
+              icon: 'google',
+              label: '구글로 계속하기',
+              borderSide: const BorderSide(),
+              btnColor: Colors.white,
+            ),
+          ],
+        ),
+      )),
+    );
+  }
+}
