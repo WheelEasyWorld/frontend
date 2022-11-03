bffdc851181b9586f66cd51e9fa03608ad008395
thing-zoo <cathy4025@gmail.com>
2022-11-03 17:02:00 +0100

    feat: add splash screen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        top: false,
+        bottom: false,
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(45.w, 0, 45.w, 0),
+          child: Center(child: Image.asset('assets/images/logo.png')),
+        ),
+      ),
+    );
+  }
+}
+
+class Init {
+  Init._();
+  static final instance = Init._();
+
+  Future initialize() async {
+    await Future.delayed(const Duration(seconds: 3));
+  }
+}
