6ad558e4e2bf49c896d97148307b4f853b0cd8e6
thing-zoo <cathy4025@gmail.com>
2022-11-04 16:02:59 +0100

    style: add green color

--- lib/screens/constants.dart
+++ lib/screens/constants.dart
@@ -7,3 +7,4 @@ const kPinkLightColor = Color(0xFFFFE8E3);
 const kYellowColor = Color(0xFFFDD69A);
 const kBrownColor = Color(0xFF857372);
 const kGreyColor = Color(0xFFECE0DF);
+const kGreenColor = Color(0xFF7EB180);
