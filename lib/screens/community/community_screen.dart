f03403e841bf3c407c20b343592f87a3c372cb5b
eunha812 <0812keh@naver.com>
2022-11-17 00:43:04 +0900

    modifying community app bar

--- lib/screens/community/community_screen.dart
+++ lib/screens/community/community_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/screens/community/components/community_appbar.dart';
 import 'package:frontend/screens/community/components/community_home_body.dart';
 import 'package:frontend/screens/constants.dart';
+import 'package:frontend/widgets/app_bar.dart';
 import 'package:get/get.dart';
 
 class CommunityScreen extends StatefulWidget {
@@ -15,7 +15,7 @@ class _CommunityScreenState extends State<CommunityScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CommunityAppbar(),
+      appBar: buildAppbar('Can Go 커뮤니티'),
       body: CommunityHomeBody(),
     );
   }
